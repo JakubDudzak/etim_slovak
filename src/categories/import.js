564781f77faf23b3a22c55ad566e90d7ff16ba6c
JakubDudzak <jakubdudzak77@gmail.com>
2023-06-03 18:11:45 +0200

    somehow somewhat

---
+++ src/categories/import.js
@@ -0,0 +1,42 @@
+const ADODB = require('node-adodb');
+const fs = require('fs');
+const ProgressBar = require('progress');
+const config = require('./config.json');
+require('dotenv').config();
+
+
+const connection = ADODB.open(`Provider=Microsoft.Jet.OLEDB.4.0;Data Source=${databasePath};`)
+
+fs.readFile(inputFile, 'utf8', async (err, data) => {
+  if (err) {
+    console.error('Failed to read file', err);
+    return;
+  }
+
+  let lines = data.split('\n');
+  let bar = new ProgressBar('Processing [:bar] :percent :etas', { total: lines.length });
+
+  for (let line of lines) {
+    let [original, translated] = line.split('___');
+
+    // Skip lines that do not have a comma or do not have a translation
+    if (!original || !translated) {
+        console.log(`Skipping invalid line: ${line}`);
+        continue;
+    }
+
+    try {
+      let escapedOriginal = original.replace(/'/g, "''");
+      let escapedTranslated = translated.replace(/'/g, "''");
+
+      await connection.execute(`UPDATE ${config.tableName} SET ${config.propertyName} = '${escapedTranslated.trim()}' WHERE ${config.propertyName} = '${escapedOriginal.trim()}'`);
+
+      bar.tick();
+      if (bar.complete) {
+        console.log('\nDone processing lines\n');
+      }
+    } catch (error) {
+      console.error(`Error processing line: `, error);
+    }
+  }
+});
